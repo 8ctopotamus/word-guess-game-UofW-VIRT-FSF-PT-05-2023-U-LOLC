f08061858917a44c4d31d653684b4b3eba54c131
Josh <zylo.codes@gmail.com>
2023-06-22 18:27:49 -0500

    Timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,18 @@
 // select dom elements
 var startButton = document.querySelector('.start-button')
 var resetButton = document.querySelector('.reset-button')
-var timerEl = document.querySelector('.timer')
+var timerEl = document.querySelector('.timer-count')
 var wordBlanksEl = document.querySelector('.word-blanks')
 var winsEl = document.querySelector('.win')
 var lossesEl = document.querySelector('.lose')
-
+var wins = 0
+var losses = 0
+var time = 10
 // list of words
 var words = ["jellyfish", "pancakes", "callback", "bacon", "porcupine"]
 var userGuesses = [] // list of letters guessed by user
 var answer
+var intervalId
 
 function getRandomWord() {
   var randomIdx = Math.floor( Math.random() * words.length )
@@ -33,10 +36,34 @@ function renderWord() {
   wordBlanksEl.innerText = blanks.split('').join(' ')
 }
 
+function tick() {
+  console.log('Tick...')
+  // check if userGuesses has all letters that are in answer string
+  // if has guess all letters
+  if (!wordBlanksEl.innerText.includes("_")) {
+    // end round
+    wins++
+    wordBlanksEl.innerText = "you won!"
+  } else {
+    // are we out of time?
+    if (time <= 0) {
+      clearInterval(intervalId)
+      timerEl.innerText = "Out of time!"
+      // diplay "lost round"
+      // increment losses
+    } else {
+      // decrement timer count
+      time--
+      // display count in page
+      timerEl.innerText = time
+    } 
+  }
+}
+
 function startRound() {
-  // TODO: start timer
   getRandomWord()
   renderWord()
+  intervalId = setInterval(tick, 1000)
 }
 
 function onKeydown(event) {
@@ -48,21 +75,7 @@ function onKeydown(event) {
   renderWord()
 }
 
-function tick() {
-  // TODO:
-  // check if userGuesses has all letters that are in answer string
-    // if has guess all letters
-      // end round
-      // increment wins
-      // display "you won!"
-    // else
-      // is our timer count at 0?
-        // if yes
-          // diplay "lost round"
-          // increment losses
-        // else
-          // decrement timer count
-}
+
 
 // Event Listeners
 startButton.addEventListener('click', startRound)
